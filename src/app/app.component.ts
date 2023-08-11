efa1ee44e48f492ac1c8765e8e0b1a313c878ba1
Sagar Ugale <sagar.ugale@innovect.com>
2023-08-11 23:35:02 +0530

    my commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  displayValue="";
+  disable = false;
+  
+  getData(val:string){
+    console.warn(val)
+    this.displayValue=val;
+  }
+  counter : number = 0;
+  add(){
+    this.counter +=1;
+  }
+  reduce(){
+    this.counter -=1;
+  }
+  changeDisable(){
+    this.disable ? this.disable=false : this.disable=true;
+  }
+  show=true;
+  show1(){
+this.show ? this.show=false : this.show=true;
+  }
 }
